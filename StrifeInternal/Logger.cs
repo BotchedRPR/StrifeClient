15675cbe40216e93a450d4177611c711c48c734f
BotchedRPR <thenxguy0@gmail.com>
2023-08-01 16:58:54 +0200

    Add GPL notice

--- StrifeInternal/Logger.cs
+++ StrifeInternal/Logger.cs
@@ -92,6 +92,7 @@ namespace StrifeClient.StrifeInternal
             // initialize the main console first. we need some output if the main window does not load for whatever reason.
             Console.WriteLine("Hi, welcome to Strife.\nSetting up class: Logger");
             System.IO.File.AppendAllText("latest.log", "Log started at " + GetCurrentDateAndTime());
+            Logger.Log("Copyright (C) 2023  BotchedRPR\r\n\r\n    This program is free software: you can redistribute it and/or modify\r\n    it under the terms of the GNU General Public License as published by\r\n    the Free Software Foundation, either version 3 of the License, or\r\n    (at your option) any later version.\r\n\r\n    This program is distributed in the hope that it will be useful,\r\n    but WITHOUT ANY WARRANTY; without even the implied warranty of\r\n    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\r\n    GNU General Public License for more details.\r\n\r\n    You should have received a copy of the GNU General Public License\r\n    along with this program.  If not, see <https://www.gnu.org/licenses/>.", LogLevel.Debug);
             Log("Class setup done", LogLevel.Debug);
         }
     }
