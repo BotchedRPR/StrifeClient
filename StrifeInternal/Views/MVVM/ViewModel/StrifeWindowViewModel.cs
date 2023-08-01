dd24e80d0f1c24fdf31c584b6d46bec2809e4535
BotchedRPR <thenxguy0@gmail.com>
2023-08-01 09:48:12 +0200

    finish mvvm, add deps

--- StrifeInternal/Views/MVVM/ViewModel/StrifeWindowViewModel.cs
+++ StrifeInternal/Views/MVVM/ViewModel/StrifeWindowViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
 namespace StrifeClient.StrifeInternal.Views.MVVM.ViewModel
 {
@@ -12,15 +14,22 @@ namespace StrifeClient.StrifeInternal.Views.MVVM.ViewModel
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
-
-        public ContactModel SelectedContact { get; set; }
-
-
-
+        public string _dmid;
+        public string DMID 
+        {
+            get { return _dmid; }
+            set { _dmid = value; }
+        }
+        public ICommand GetChannelFromApi { get; private set; }
+        public void test()
+        {
+            Logger.Log("Hello from mvvm buttonm, dmid is " + _dmid, Logger.LogLevel.Debug);
+        }
         public StrifeWindowViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
+            GetChannelFromApi = new RelayCommand(test);
 
             Messages.Add(new MessageModel
             {
